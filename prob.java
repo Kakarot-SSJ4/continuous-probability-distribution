f299fbcbc92600bf838be59adfca11e1f7e3de49
Kakarot-SSJ4 <46711393+Kakarot-SSJ4@users.noreply.github.com>
2019-03-25 12:48:17 +0530

    initialized the project

---
+++ prob.java
@@ -0,0 +1,12 @@
+/* This program gives the probability in a certain interval of a continuous probability distribution where the integral is not solvable, namely - 
+Standard normal distribution
+Gamma distribution
+Lognormal distribution
+Beta distribution
+*/
+import java.util.*;
+class prob{
+public double integrate_normal(){
+
+}
+}
